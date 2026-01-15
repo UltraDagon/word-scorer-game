f1ae14b3469d4513dbcd4ae8f1e3c36500593b1e
UltraDagon <43974011+UltraDagon@users.noreply.github.com>
2026-01-15 02:28:08 -0600

    updated news

--- frontend/src/components/News.tsx
+++ frontend/src/components/News.tsx
@@ -5,10 +5,23 @@ export function News() {
     <div className="news">
       <h1 className="news-title">News</h1>
       <p>
-        1/14/2026
+        1/15/2026
         <br />
-        This is just here to show how the news entries will look. Will be
-        removed later.
+        Ran another playtest! Steps left before beta release: create a
+        bug/suggestion form, change website address, websocket security checks
+        (especially for score).
+        <br />
+        Features and bugs:
+        <br />
+        - Added the feature for the 2nd earliest player in a lobby to be able to
+        kick the owner if the owner disconnects.
+        <br />- Bug: Tiles placed on the far left column would split in to two
+        words. Ex: COW --{">"} COW & OW.
+        <br />
+        - Bug: Claiming a user after a disconnect would lead to a shift in the
+        user order, possibly leading to someone's turn being skipped.
+        <br />- Bug: The "Kick User" button would not appear to the owner of the
+        lobby when it was supposed to.
       </p>
       <p>
         1/14/2026
